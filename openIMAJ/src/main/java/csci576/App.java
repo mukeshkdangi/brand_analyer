02928347fffceba1dd4d04c1500f3af02f3261d0
Omar Skalli <oskalli@andrew.cmu.edu>
2019-04-21 14:55:57 -0700

    Updated threshold values

--- openIMAJ/src/main/java/csci576/App.java
+++ openIMAJ/src/main/java/csci576/App.java
@@ -1,18 +1,38 @@
 package csci576;
 
+import java.awt.Toolkit;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.pixel.statistics.HistogramModel;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.image.renderer.MBFImageRenderer;
 import org.openimaj.video.VideoDisplay;
 import org.openimaj.video.xuggle.XuggleVideo;
+
+import com.lowagie.text.Rectangle;
+
 import org.openimaj.math.geometry.point.Point2d;
+import org.openimaj.math.geometry.shape.Shape;
 import org.openimaj.math.statistics.distribution.MultidimensionalHistogram;
 import org.openimaj.video.VideoDisplayListener;
 import org.openimaj.video.processing.motion.GridMotionEstimator;
 import org.openimaj.video.processing.motion.MotionEstimator;
 import org.openimaj.video.processing.motion.MotionEstimatorAlgorithm;
+import org.openimaj.video.processing.shotdetector.CombiShotDetector;
+import org.openimaj.video.processing.shotdetector.HistogramVideoShotDetector;
+import org.openimaj.video.processing.shotdetector.LocalHistogramVideoShotDetector;
+import org.openimaj.video.processing.shotdetector.ShotBoundary;
+import org.openimaj.video.processing.shotdetector.ShotDetectedListener;
+import org.openimaj.video.processing.shotdetector.VideoKeyframe;
+import org.openimaj.video.timecode.VideoTimecode;
 import org.openimaj.video.translator.FImageToMBFImageVideoTranslator;
 import org.openimaj.video.translator.MBFImageToFImageVideoTranslator;
+import org.openimaj.demos.video.VideoShotDetectorVisualisation;
+import org.openimaj.feature.DoubleFV;
 import org.openimaj.feature.DoubleFVComparison;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
@@ -70,9 +90,9 @@ public class App {
 	 * 
 	 */
 	
-	public static MultidimensionalHistogram generateHistogram(MBFImage frame) {
+	public static MultidimensionalHistogram generateHistogram(MBFImage frame, int nBins) {
 		
-		HistogramModel model = new HistogramModel(2, 2, 2);
+		HistogramModel model = new HistogramModel(8, 8, 8);
 		model.estimateModel(frame);
 		MultidimensionalHistogram histogram = model.histogram;
 		
@@ -115,42 +135,58 @@ public class App {
 	 * 
 	 */
 	
-	
-	public static void shotBoundaryDetector(XuggleVideo video, float threshold){
+	public static List<Float> shotBoundaryDetector(XuggleVideo video, float threshold, int minFrames){
 		
-		// get the first frame
+		// Get the first frame
 		MBFImage lastFrame = video.getNextFrame();
-		MultidimensionalHistogram lastHistogram = generateHistogram(lastFrame);
 		FImage last = lastFrame.flatten();
 		
-		//Initialize frame count
+		// Initialize frame count
 		float lastFrameNo = 0;
 		float currentFrameNo = 1;
 		
-		// iterate through the frames
+		// Initialize histogram
+		int nBins = video.getWidth();
+		MultidimensionalHistogram lastHistogram = generateHistogram(lastFrame, nBins);
+		
+		// Initialize output list
+		List<Float> outputList = new ArrayList<Float>();
+		outputList.add(lastFrameNo);
+		
+		// Iterate through the frames
 		for (final MBFImage currentFrame : video) {
 			final FImage current = currentFrame.flatten();
 			
 			float meanVal = meanSquareDifference(current, last);
 			
-			MultidimensionalHistogram currentHistogram = generateHistogram(currentFrame);
+			MultidimensionalHistogram currentHistogram = generateHistogram(currentFrame, nBins);
 			double distanceScore = currentHistogram.compare(lastHistogram, DoubleFVComparison.EUCLIDEAN);
 			
 			//System.out.println("Current Histogram: " + currentHistogram.toString());
 			//System.out.println("Last Histogram: " + lastHistogram.toString());
 			
-			// might need adjust threshold:
+			// Might need adjust threshold:
 			if (meanVal > threshold && distanceScore > threshold) {
-				System.out.println("New Shot detected");
-				DisplayUtilities.displayName(current, "debugCurrent");
-				DisplayUtilities.displayName(last, "debugLast");
-
+				
+				float lastShotStart = outputList.get(outputList.size() - 1);
+				
+				System.out.println("Frames in between: " + (lastFrameNo - lastShotStart));
+				
+				if ((lastFrameNo - lastShotStart) >  minFrames) {
+				
+					System.out.println("New Shot detected");
+					DisplayUtilities.displayName(current, "debug: Current Frame");
+					DisplayUtilities.displayName(last, "debug: Last Frame");					
+					outputList.add(lastFrameNo);
+					System.out.println("List: " + outputList.toString());
+					
+				}
+				
 			}
 			
 			System.out.println("From frame " + lastFrameNo + ", to " + currentFrameNo + ", meanVal = " + meanVal + ", histogramScore = " + distanceScore);					
 			
-			// set the current frame to the last frame
-			
+			// Set the current frame to the last frame
 			last = current;
 			lastFrame = currentFrame;
 			lastHistogram = currentHistogram;
@@ -160,6 +196,7 @@ public class App {
 			
 		}
 						
+		return outputList;
 		
 	}
 	
@@ -170,8 +207,13 @@ public class App {
     	
     	//VideoDisplay<MBFImage> display = VideoDisplay.createVideoDisplay(video);
     	
-    	shotBoundaryDetector(video, 0.055f);
+    	List<Float> frameBoundary = shotBoundaryDetector(video, 0.07f, 5);
 		
+    	System.out.println(frameBoundary);
+    	
+    	//new VideoFeatureExtraction();
+    	
+    	
 //    	for (MBFImage mbfImage : video) {
 //    	    DisplayUtilities.displayName(mbfImage.process(new CannyEdgeDetector()), "videoFrames");
 //    	}
